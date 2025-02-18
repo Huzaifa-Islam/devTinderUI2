b949b1609a36b79ebccecbcc921452679f5df6dd
Huzaifa-Islam <official.huzaifa.coder@gmail.com>
2025-02-18 11:55:06 +0530

    Tried Routing

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - try some daisyUI components
 
 - create a NavBar.jsx file and import that in app.js file 
+
+- Lets create a routing feature.. as we cannot show everything on one page..
+- we will use react browser router -- read the documentation --> npm i react-router-dom
+- we will use this react router dom in the app.jsx file and define the routers and element/component/code to show on that path 
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function App() {
   return (
     <>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<div>Base Page</div>} />
+        <Route path="/login" element={<div>Login Page</div>} />
+        <Route path="/test" element={<div>test page</div>} />
+      </Routes>
+    </BrowserRouter>
       <NavBar/>
       <h1 className="text-3xl font-bold">Hello</h1>
     </>
